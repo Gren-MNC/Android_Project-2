fa38c67eee3585ccb453f49cf21b5694cf009dea
nmc0401 <nmc0401@gmail.com>
2023-03-18 02:25:15 +0700

    add search/album/topic/playlist Layout

---
+++ app/src/main/java/com/example/layoutservice/Activity/AllAlbumListActivity.java
@@ -0,0 +1,15 @@
+package com.example.layoutservice.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+
+public class AllAlbumListActivity extends AppCompatActivity {
+
+}
