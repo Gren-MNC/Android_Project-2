5a7fd89315e753bb57d5594a7b463a57c039bcc2
Phan Đại Đăng Khoa <111940212+pddkhoa@users.noreply.github.com>
2023-04-16 18:45:59 +0700

    remove miniplayer

--- app/src/main/java/com/example/layoutservice/Activity/MainActivity.java
+++ app/src/main/java/com/example/layoutservice/Activity/MainActivity.java
@@ -24,57 +24,15 @@ public class MainActivity extends AppCompatActivity {
     Button btnStart;
     Button btnStop;
     private TabLayout mtabLayout;
-    public static String value = null;
-    public static boolean SHOW_MINI_PLAYER = false;
-    public static int STATUS_MUSIC;
-    public static int actionM;
-    public static String SONG_NAME = null;
-    private int actionService, mCurrentPosition, durationTotal, positionSelect;
-    ArrayList<SongFireBase> listSong;
-    private SongFireBase mSong;
-    public static boolean IS_PLAYING;
-    public static boolean isIsPlaying;
 
 
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle bundle = intent.getExtras();
-            if(bundle == null)
-            {
-                return;
-            }
 
-            value = bundle.getString("tenbaihat");
-            actionM = bundle.getInt("action_music");
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_main);
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
-                new IntentFilter("send_data_to_activity"));
-                Log.e("Test",""+STATUS_MUSIC);
 
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if(value!=null){
-            SONG_NAME = value;
-            SHOW_MINI_PLAYER = true;
-            STATUS_MUSIC=actionM;
-            Log.e("Mess",""+STATUS_MUSIC);
-        }
-        else {
-            SHOW_MINI_PLAYER = false;
-            SONG_NAME = null;
-        }
     }
 
 }
